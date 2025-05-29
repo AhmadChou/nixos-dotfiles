6b09d6e48dc075be34513cf66aee18302e1c22b1
AhmadChou <ahmad.choudhry17@gmail.com>
2025-05-30 00:23:03 +1000

    fixed typo in hyprland config

--- programs/hyprland.nix
+++ programs/hyprland.nix
@@ -162,7 +162,7 @@
       bindel = [
         ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+"
         ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-"
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIOSINK@ toggle"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle "
         ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
         ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
